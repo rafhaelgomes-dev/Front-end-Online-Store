b8f57dbd48d13f61113d8b7a49aa02d3fbe3ad9d
Daniloisaac <daniloisaac942@gmail.com>
2022-08-05 18:45:29 -0300

    adicionei o requisito 5

--- src/pages/ListProducts.jsx
+++ src/pages/ListProducts.jsx
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class ListProducts extends Component {
   constructor() {
     super();
     this.state = {
       listProducts: [],
+      inputSearch: '',
+      button: false,
     };
   }
 
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSearchButton = async () => {
+    const { inputSearch } = this.state;
+    const fetchApi = await getProductsFromCategoryAndQuery('', inputSearch);
+    this.setState({ listProducts: fetchApi.results, button: true });
+  }
+
   render() {
-    const { listProducts } = this.state;
+    const { listProducts, inputSearch, button } = this.state;
     return (
       <div>
-        <h1>Lista de Produtos</h1>
+        <input
+          name="inputSearch"
+          value={ inputSearch }
+          onChange={ this.handleChange }
+          placeholder="pesquisa"
+          data-testid="query-input"
+          type="text"
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleSearchButton }
+        >
+          pesquisar
+        </button>
         {
-          !listProducts.length
-        && (
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
-        )
+          !button
+            ? (
+              <p data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>
+            ) : !listProducts.length && <p>Nenhum produto foi encontrado</p>
+        }
+        {
+          button && (
+            listProducts.map((listProduct) => (
+              <div key={ listProduct.id } data-testid="product">
+                <p>{listProduct.title}</p>
+                <img
+                  src={ listProduct.thumbnail }
+                  alt={ listProduct.id }
+                />
+                <p>{`preço: R$ ${listProduct.price}`}</p>
+              </div>
+            ))
+          )
         }
       </div>
     );
