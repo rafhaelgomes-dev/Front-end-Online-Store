25894d41a3f55a42ad0d01ec4684d6c1e171b24d
pedroygor <pedroygor2010@hotmail.com>
2022-08-05 15:33:38 -0300

    Começando Requisito 2

---
+++ src/pages/ListProducts.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ListProducts extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lista de Produtos</h1>
+      </div>
+    );
+  }
+}
