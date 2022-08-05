9a2f607c3b48e0e360b299647f481ecd0621841a
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-05 16:01:31 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { getProductsFromCategoryAndQuery } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // componentDidMount() {
+  //   getProductsFromCategoryAndQuery(undefined, 'computador');
+  // }
+
+  render() {
+    return (
+      <div>
+        <p>...</p>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (query === undefined) {
+    const responseCategory = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const dataCategory = await responseCategory.json();
+    return dataCategory;
+  }
+  const responseQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const dataQuery = await responseQuery.json();
+  return dataQuery;
 }
