0ea12601898cd7dab11756b525080c342df5177a
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-05 16:04:48 -0300

    Requisito 1 restaura App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-// import { getProductsFromCategoryAndQuery } from './services/api';
 
-class App extends React.Component {
-  // componentDidMount() {
-  //   getProductsFromCategoryAndQuery(undefined, 'computador');
-  // }
-
-  render() {
-    return (
-      <div>
-        <p>...</p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>Edit src/App.js and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
