607d3a1efc37d767d9e0378f6723ee00269a3fc8
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-08 21:13:51 -0300

    requisito 10 teste

--- src/pages/Shoppingcart.jsx
+++ src/pages/Shoppingcart.jsx
@@ -6,12 +6,14 @@ export default class Shoppingcart extends React.Component {
     super();
     this.state = {
       shoppingCartList: [],
-      // buttonDisable: [],
+      buttonDisable: [],
+      produtosNumeros: [],
     };
   }
 
   componentDidMount() {
     this.listAddState();
+    this.teste();
   }
 
      listAddState = () => {
@@ -21,11 +23,22 @@ export default class Shoppingcart extends React.Component {
        });
      }
 
-    totalProducts = (id) => {
+    teste = () => {
       const { listItemsAdd } = this.props;
       const newArray = [...listItemsAdd];
-      const arrayFilter = newArray.filter((e) => e.id === id);
-      return arrayFilter.length;
+      newArray.forEach(() => {
+        this.setState((preventState) => (
+          { produtosNumeros: [...preventState.produtosNumeros, 1] }));
+      });
+    }
+
+    totalProducts = (id) => {
+      const { produtosNumeros } = this.state;
+      const newArray = [...produtosNumeros];
+      newArray[id] += 1;
+      this.setState({
+        produtosNumeros: [...newArray],
+      });
     };
 
     addNewProducts = (produt) => {
@@ -33,31 +46,36 @@ export default class Shoppingcart extends React.Component {
         { shoppingCartList: [...preventState.shoppingCartList, produt] }));
     }
 
-    // removeAddProducts = (id) => {
-    //   const { listItemsAdd } = this.props;
-    //   const { getPropsOfChildrensDelete } = this.props;
-    //   const newArray = [...listItemsAdd];
-    //   for (let index = 0; index < newArray.length; index += 1) {
-    //     if (listItemsAdd[index].id === id) {
-    //       newArray.splice(index, 1);
-    //       const filter = newArray.filter((e) => e.id === id);
-    //       if (filter.length <= 0) {
-    //         return;
-    //       }
-    //       getPropsOfChildrensDelete(newArray);
-    //       return;
-    //     }
-    //   }
-    // }
+    removeAddProducts = (id) => {
+      const { produtosNumeros } = this.state;
+      const newArray = [...produtosNumeros];
+      if (newArray[id] <= 1) {
+        return;
+      }
+      newArray[id] -= 1;
+      this.setState({
+        produtosNumeros: [...newArray],
+      });
+    }
+
+    removerProduto = (i) => {
+      const { listItemsAdd } = this.props;
+      const { getPropsOfChildrensDelete } = this.props;
+      const newArray = [...listItemsAdd];
+      newArray.splice(i, 1);
+      getPropsOfChildrensDelete(newArray);
+    }
 
     render() {
-      const { listItemsAdd, getPropsOfChildrens } = this.props;
-      // const { buttonDisable } = this.state;
+      const { listItemsAdd } = this.props;
+      const { buttonDisable, produtosNumeros } = this.state;
 
       return (
         <div>
           {listItemsAdd.length === 0 ? (
-            <p data-testid="shopping-cart-empty-message">Seu carrinho está  vazio</p>
+            <div>
+              <p data-testid="shopping-cart-empty-message">Seu carrinho está  vazio</p>
+            </div>
           ) : (
             <div>
               <p>
@@ -73,32 +91,40 @@ export default class Shoppingcart extends React.Component {
                         alt={ listItemAdd.id }
                       />
                       <p>{`preço: R$ ${listItemAdd.price}`}</p>
+                      <p>
+                        Total:
+                        {' '}
+                        {produtosNumeros[i]}
+                      </p>
                       <button
                         data-testid="product-increase-quantity"
                         type="button"
-                        onClick={ () => getPropsOfChildrens(listItemAdd) }
+                        onClick={ () => this
+                          .totalProducts(i) }
                       >
-                        ADD+
-                        {' '}
+                        +
                       </button>
-                      {/* <button
+                      <button
                         data-testid="product-decrease-quantity"
                         disabled={ buttonDisable[i] }
                         type="button"
-                        onClick={ () => this.removeAddProducts(listItemAdd.id) }
+                        onClick={ () => this.removeAddProducts(i) }
                       >
-                        {' '}
-                        REMOVE-
-                        {' '}
-
-                      </button> */}
+                        -
+                      </button>
+                      <button
+                        data-testid="remove-product"
+                        onClick={ () => this.removerProduto(i) }
+                        type="button"
+                      >
+                        Remover Produto
+                      </button>
+                      <p data-testid="shopping-cart-product-quantity">
+                        {`quantidade: ${produtosNumeros[i]}`}
+                      </p>
                     </div>
                   ))
               }
-              <p data-testid="shopping-cart-product-quantity">
-                {`quantidade: ${listItemsAdd.length}`}
-              </p>
-
             </div>
           )}
         </div>
@@ -111,6 +137,5 @@ Shoppingcart.propTypes = {
     map: PropTypes.func.isRequired,
     length: PropTypes.func.isRequired,
   }).isRequired,
-  getPropsOfChildrens: PropTypes.func.isRequired,
-  // getPropsOfChildrensDelete: PropTypes.func.isRequired,
+  getPropsOfChildrensDelete: PropTypes.func.isRequired,
 };
