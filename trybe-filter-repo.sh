135a7d53ba3c3f39f9e5fe52ba88997334e57ddd
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-08-11 23:54:47 -0300

    envio para o github

---
+++ trybe-filter-repo.sh
@@ -0,0 +1,23 @@
+### GIT FILTER-REPO ###
+
+## NÃO EXECUTE ESSE SCRIPT DIRETAMENTE
+## Esse script foi feito para uso do
+## script 'publisher.sh' fornecido 
+## pela Trybe. 
+
+[[ $# == 1 ]] && \
+[[ $1 == "trybe-security-parameter" ]] && \
+git filter-repo \
+    --path .trybe \
+    --path .github \
+    --path trybe.yml \
+    --path trybe-filter-repo.sh \
+    --path src/__tests__ \
+    --path src/__mocks__ \
+    --path it-only.png \
+    --path pull-request-para-branch-do-grupo.png \
+    --path only-one-green.png \
+    --path wireframes \
+    --path trello.gif \
+    --path README.md \
+    --invert-paths --force
